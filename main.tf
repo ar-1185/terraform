3e6ac3d234e5ce4fa2ed17a863985d9998699e2b
Arpit Sharma <65287674+ar-1185@users.noreply.github.com>
2022-09-26 11:26:12 +0530

    Update main.tf
    For google cloud

--- main.tf
+++ main.tf
@@ -9,8 +9,58 @@ provider "google" {
   version = "3.5.0"
   project = "<PROJECT_ID>"
   region  = "us-central1"
-  zone    = "us-central1-c"
+  zone    = "us-central1-c" 
 }
+
+
+#Creating VPC Network
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+#Creating VM Instance
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+
+
+# New resource for the storage bucket our application will use.
+resource "google_storage_bucket" "example_bucket" {
+  name     = "<UNIQUE-BUCKET-NAME>"
+  location = "US"
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# Create a new instance that uses the bucket
+resource "google_compute_instance" "another_instance" {
+  # Tells Terraform that this VM instance must be created only after the
+  # storage bucket has been created.
+  depends_on = [google_storage_bucket.example_bucket]
+  name         = "terraform-instance-2"
+  machine_type = "f1-micro"
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
 }
